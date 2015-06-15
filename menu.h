18bee2930f4c0131f78ba3940095d6acb4a56a6d
Alfred Ly <alfredly96@gmail.com>
2015-06-15 10:42:43 -0700

    Update menu.h

--- menu.h
+++ menu.h
@@ -4,59 +4,68 @@
 #include <iomanip>
 #include "HamiltonianCircuit.h"
 #include "LinkedGraph.h"
+#include "input.h"
 
 using namespace std;
 
+template <class Labeltype>
 class Menu {
 
+private:
+//	HamiltonianCircuit<Labeltype> theGraph;
 public:
-	//	static void Initialize();
-	HamiltonianCircuit<class LabelType> theGraph;
 	Input input;
 	virtual void displayHeader();       //Top portion of our user interface display
 	virtual void displayBody();			// The raw menu content
 	virtual void displayFooter();
 };
 
-class MenuRead : public Menu {
+template <class Labeltype>
+class MenuRead : public Menu<Labeltype> {
 public:
 	void displayHeader();
-	void displayBody();
+	void displayBody(const Labeltype& theGraph);
 };
 
-class MenuAdd : public Menu {
+template <class Labeltype>
+class MenuAdd : public Menu<Labeltype> {
 public:
 	void displayHeader();
-	void displayBody();
+	void displayBody(const Labeltype& theGraph);
 };
 
-class MenuRemove : public Menu {
+template <class Labeltype>
+class MenuRemove : public Menu<Labeltype> {
 public:
 	void displayHeader();
-	void displayBody();
+	void displayBody(const Labeltype& theGraph);
 };
 
-class MenuUndo : public Menu {
+template <class Labeltype>
+class MenuUndo : public Menu<Labeltype> {
 public:
 	void displayHeader();
-	void displayBody();
+	void displayBody(const Labeltype& theGraph);
 };
 
-class MenuDisplay : public Menu {
+template <class Labeltype>
+class MenuDisplay : public Menu<Labeltype> {
 public:
 	void displayHeader();
-	void displayBody();
-	void displayHelper(LabelType& label);
+	void displayBody(const Labeltype& theGraph);
+	void displayHelper(const Labeltype& label);
 };
 
-class MenuSolve : public Menu {
+template <class Labeltype>
+class MenuSolve : public Menu<Labeltype> {
 public:
 	void displayHeader();
-	void displayBody();
+	void displayBody(const Labeltype& theGraph);
 };
 
-class MenuWrite : public Menu {
+template <class Labeltype>
+class MenuWrite : public Menu<Labeltype> {
 public:
 	void displayHeader();
-	void displayBody();
+	void displayBody(const Labeltype& theGraph);
 };
