c3ebfe20731a1b65b99e22e265a1e46fa76c2e95
Alfred Ly <alfredly96@gmail.com>
2015-06-18 17:27:31 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
-#include "menu.h"
+#include "Menu.h"
 #include "HamiltonianCircuit.h"
-#include "GraphInterface.h"
-#include "input.h"
+#include "Input.h"
 #include <windows.h>
 #include <cstdlib>
 using namespace std;
@@ -24,6 +23,10 @@ using namespace std;
 #define STATE_READ		7
 #define STATE_WRITE		8
 
+void displayHelper(string& label)
+{
+	cout << label << " ";
+}
 
 int main() {
 
@@ -40,23 +43,23 @@ int main() {
 	MenuRead<string>			menu_Read;
 	MenuWrite<string>			menu_Write;
 
-		//Main globals
+	//Main globals
 	int state = 0;			// 0 - splash page
-							// 1 - main menu	
-							// 2 - add menu
-							// 3 - remove menu
-							// 4 - undo menu
-							// 5 - display menu
-							// 6 - solve menu
-							// 7 - read menu
-							// 8 - write menu
-							// 9 - quit option
-							// 10- dance party!!!
+	// 1 - main menu	
+	// 2 - add menu
+	// 3 - remove menu
+	// 4 - undo menu
+	// 5 - display menu
+	// 6 - solve menu
+	// 7 - read menu
+	// 8 - write menu
+	// 9 - quit option
+	// 10- dance party!!!
 	int prev_state = 0;
 
 	int substate = 0;		// 0 - None
-							// 1 - Depth First Search
-							// 2 - Breadth First Search
+	// 1 - Depth First Search
+	// 2 - Breadth First Search
 
 	bool quit = false;
 
@@ -64,151 +67,191 @@ int main() {
 
 		//Prompt program splash
 		switch (state) {
-			case 0: //slash menu
-				cout << "CIS 22C Team 6 Group Project: " << endl;
-				cout << "Hamiltonian Circut" << endl;
-				cout << "Press any key to continue..." << endl;
-				menu_Main.displayFooter();
+		case 0: //slash menu
+			cout << "CIS 22C Team 6 Group Project: " << endl;
+			cout << "Hamiltonian Circut" << endl;
+			cout << "Press any key to continue..." << endl;
 
-				input.getCh();  // wait for any key
-				state = STATE_MAIN; // switch state to main menu
-				break;
-			case 1: // case state main menu
-				menu_Main.displayHeader(); // display main menu header
-				menu_Main.displayBody();   // display main menu body
-				menu_Main.displayFooter(); // display main menu footer
 
-				prev_state = 1; // set last state to main menu
-				cout << "Input your choice" << endl;
-				switch (input.getCh())	{	//switch input
+			input.getCh();  // wait for any key
+			state = STATE_MAIN; // switch state to main menu
+			break;
+		case 1: // case state main menu
+			menu_Main.displayHeader(); // display main menu header
+			menu_Main.displayBody(circuit);   // display main menu body
 
-					case 1: // add menu
-						state = 2;
-						break;
-					case 2: //remove menu
-						state = 3;
-						break;
-					case 3: //undo menu
-						state = 4;
-						break;
-					case 4: //display menu
-						state = 5;
-						break;
-					case 5: //solve menu
-						state = 6;
-						break;
-					case 6:	//read menu
-						state = 7;
-						break;
-					case 7:	//write party
-						state = 8;
-						break;
-					case 8:	//quit option
-						state = 9;
-						break;
-					case 9: // dance party
-						state = 10;
-						break;
-				}
-				break;
-			case 2:		// case state add menu
-				menu_Add.displayHeader();
-				menu_Add.displayBody();
+			prev_state = 1; // set last state to main menu
+			cout << "Input your choice" << endl;
+			cin >> state;
+			switch (state)	{	//switch input
 
-				prev_state = 2;		//set last state to add
-				switch (input.getCh()){
-					case 1:
-						state = 1; //back to main menu
-						break;
-				}
+			case 1: // add menu
+				state = 2;
 				break;
-			case 3:		// case state for remove menu
-				menu_Remove.displayHeader();
-				menu_Remove.displayBody(circuit);
+			case 2: //remove menu
+				state = 3;
+				break;
+			case 3: //undo menu
+				state = 4;
+				break;
+			case 4: //display menu
+				state = 5;
+				break;
+			case 5: //solve menu
+				state = 6;
+				break;
+			case 6:	//read menu
+				state = 7;
+				break;
+			case 7:	//write party
+				state = 8;
+				break;
+			case 8:	//quit option
+				state = 9;
+				break;
+			case 9: // dance party
+				state = 10;
+				break;
+			}
+			break;
+		case 2:		// case state add menu
+			menu_Add.displayHeader();
+			menu_Add.displayBody(circuit);
 
-				prev_state = 3;		//set last state to remove
-				switch (input.getCh()){
-					case 1:
-						state = 1; //back to main menu
-						break;
-				}
-				break;
-			case 4:		// case state for undo menu
-				menu_Undo.displayHeader();
-				menu_Undo.displayBody(circuit);
+			prev_state = 2;		//set last state to add
+			cout << "Press 1 to go back to main menu, or any other key to add more " << endl;
+			cin >> state;
+			switch (state){
+				case 1:
+					state = 1; //back to main menu
+					break;
+				default:
+					state = 2; //back to add menu
+			}
+			break;
+		case 3:		// case state for remove menu
+			menu_Remove.displayHeader();
+			menu_Remove.displayBody(circuit);
 
-				prev_state = 4;		//set last state to undo
-				switch (input.getCh()){
-					case 1:
-						state = 1; //back to main menu
-						break;
-				}
-				break;
-			case 5:		// case state for display menu
-				menu_Display.displayHeader();	
-				menu_Display.displayBody(circuit);
+			prev_state = 3;		//set last state to remove
+			cout << "Press 1 to go back to main menu, or any other key to remove more " << endl;
+			cin >> state;
+			switch (state){
+				case 1:
+					state = 1; //back to main menu
+					break;
+				default:
+					state = 3; //back to remove menu
+			}
+			break;
+		case 4:		// case state for undo menu
+			menu_Undo.displayHeader();
+			menu_Undo.displayBody(circuit);
 
-				prev_state = 5;		//set last state to display
-				switch (input.getCh()) {
-					case 1: // main menu
-						state  = 1;
-						break;
-					
-				}
-				break;
-			case 6:			//case state for solve menu
-				menu_Solve.displayHeader();
-				menu_Solve.displayBody(circuit);
-				
-				prev_state = 6;		//set last state to solve
-				switch (input.getCh()){
-					case 1:
-						state = 1; //back to main menu
-						break;
-				}
-				break;
-			case 7:
-				menu_Read.displayHeader();
-				menu_Read.displayBody(circuit);
+			prev_state = 4;		//set last state to undo
+			cout << "Press 1 to go back to main menu" << endl;
+			cin >> state;
+			switch (state){
+				case 1:
+					state = 1; //back to main menu
+					break;
+				default:
+					cout << "You didn't press 1...going back to the main menu anyway..." << endl;
+					state = 1; //back to main menu
+			}
+			break;
+		case 5:		// case state for display menu
+			menu_Display.displayHeader();
+			menu_Display.displayBody(circuit);
 
-				prev_state = 7;
-				switch (input.getCh()){
-					case 1:
-						state = 1;	// back to main menu
-						break;
-				}
+			prev_state = 5;		//set last state to display
+			cin >> state;
+			switch (state) {
+				case 1:
+					circuit->depthFirstTraversalH(displayHelper);
+					cout << endl;
+					system("pause");
+					break;
+				case 2:
+					circuit->breadthFirstTraversalH(displayHelper);
+					cout << endl;
+					system("pause");
+					break;
+				case 3: // main menu
+					state = 1;
+					break;
+				default:
+					cout << "Invalid Option. " << endl;
+					state = 5; //back to display menu
+			}
+			break;
+		case 6:			//case state for solve menu
+			menu_Solve.displayHeader();
+			menu_Solve.displayBody(circuit);
+			prev_state = 6;		//set last state to solve
+			cout << "Press 1 to go back to main menu" << endl;
+			cin >> state;
+			switch (state) {
+			case 1: // main menu
+				state = 1;
 				break;
-			case 8:
-				menu_Write.displayHeader();
-				menu_Read.displayBody(circuit);
+			default:
+				cout << "You didn't press 1...going back to the main menu anyway..." << endl;
+				state = 1; //back to main menu
+			}
+			break;
+		case 7:
+			menu_Read.displayHeader();
+			menu_Read.displayBody(circuit);
 
-				prev_state = 8;
-				switch (input.getCh()){
-					case 1:
-						state = 1;  // back to main menu
-						break;
-				}
-				break;
-			case 9:			//quit quit
-				quit = true;
-				break;
-			case 10:
-				system(PCLEAR);
-				menu_Main.displayHeader();
-				cout << "ARE YOU READY? Y/N?" << endl;
-				menu_Main.displayFooter();
+			prev_state = 7;
+			cout << "Press 1 to go back to main menu" << endl;
+			cin >> state;
+			switch (state) {
+				case 1: // main menu
+					state = 1;
+					break;
+				default:
+					cout << "You didn't press 1...going back to the main menu anyway..." << endl;
+					state = 1; //back to main menu
+			}
+			break;
+		case 8:
+			menu_Write.displayHeader();
+			menu_Write.displayBody(circuit);
 
-				switch (input.getCh()) {
-					case 'y':
-					case 'Y':
-						ShellExecute(NULL, "open", "http://corgiorgy.com", NULL, NULL, SW_SHOWNORMAL);
-					case 'n':
-					case 'N':
-						state = prev_state;
-				}
+			prev_state = 8;
+			cout << "Press 1 to go back to main menu" << endl;
+			cin >> state;
+			switch (state) {
+				case 1: // main menu
+					state = 1;
+					break;
+				default:
+					cout << "You didn't press 1...going back to the main menu anyway..." << endl;
+					state = 1; //back to main menu
+			}
+			break;
+		case 9:			//quit quit
+			quit = true;
+			break;
+		case 10:
+			system(PCLEAR);
+			menu_Main.displayHeader();
+			cout << "ARE YOU READY? Y/N?" << endl;
 
-				break;
+			switch (input.getCh()) {
+			case 'y':
+			case 'Y':
+				ShellExecute(NULL, "open", "http://corgiorgy.com", NULL, NULL, SW_SHOWNORMAL);
+			case 'n':
+			case 'N':
+				state = prev_state;
+			}
+
+			break;
 		}
 	}
 	return 0;
 }
+
